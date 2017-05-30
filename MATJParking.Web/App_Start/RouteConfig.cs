3e25dbac6b8c3cc56d245e8d341280a532ef50b3
ecknomichiel <michiel@frostvikenit.eu>
2017-05-30 16:25:41 +0200

    First steps towards Checkin

--- MATJParking.Web/App_Start/RouteConfig.cs
+++ MATJParking.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MATJParking.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Vehicle", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- MATJParking.Web/Controllers/VehicleController.cs
+++ MATJParking.Web/Controllers/VehicleController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MATJParking.Web.Repositories;
+using MATJParking.Web.Models;
 
 namespace MATJParking.Web.Controllers
 {
@@ -13,5 +15,31 @@ namespace MATJParking.Web.Controllers
         {
             return View();
         }
+
+        public ActionResult CheckIn(string registrationNumber, string vehicleTypeId)
+        {
+            int type=0;
+            if ("" != vehicleTypeId)
+                type = int.Parse(vehicleTypeId);
+            Vehicle vehicle = Garage.Instance.SearchVehicle(registrationNumber); 
+            ParkingPlace pl = null;
+
+            try 
+	        {	        
+		        pl = Garage.Instance.CheckIn(registrationNumber, type);
+	        }
+	        catch (Exception e)
+	        {
+		        //No space available or vehicle already parked
+                ViewBag.Message = e.Message;
+                //Add a dummy parkingplace with all available vehicle info
+                pl = new ParkingPlace();
+                if (vehicle != null)
+                    pl.Park(vehicle);
+	        }
+
+            return View(pl);    
+        }
     }
+
 }
